fe8ac3dde98d185d38cb95f186dad93713b6f8a7
eyal.dan <danba2323@gmail.com>
2023-08-30 17:48:35 +0300

    feature: new API

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ docker pull danos23/java-ci-cd:latest
 docker stop simple-java-maven-app || true
 docker rm simple-java-maven-app || true
 # Run the Docker container
-docker run -d --name simple-java-maven-app -p 80:8080 danos23/java-ci-cd:latest
+docker run -d --name eyal-simple-hello -p 80:8080 danos23/java-ci-cd:latest
